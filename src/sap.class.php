21bbe8e5d1a007c8d37b27f4addfa70f01a2af8e
Robert Fels <robert.fels@contiva.com>
2022-06-04 21:47:19 +0200

    new

---
+++ src/sap.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Contiva\SapCpiHelper;
+class SapCpiHelper {
+    public $aMemberVar = 'aMemberVar Member Variable';
+    public $aFuncName = 'aMemberFunc';
+   
+    function aMemberFunc() {
+        print 'Inside `aMemberFunc()`';
+    }
+}
